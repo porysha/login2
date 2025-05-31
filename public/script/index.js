7bafd741ee70cb08fbac8a7d9db9ebfa695eb5e6
Porisha ( Poriya Bagheri ) <114158866+porysha@users.noreply.github.com>
2025-05-31 12:44:23 +0330

    Update index.js

--- public/script/index.js
+++ public/script/index.js
@@ -14,7 +14,7 @@ window.addEventListener("load", (e) => {
     let isLog = getCookie("email");
     if (!isLog) {
         console.log("ثبت نام انجام نشد");
-        location.href = "http://localhost:3000/public/login.html";
+        location.href = "/login.html";
     } else {
         console.log("ثبت نام انجام شد");
     }
